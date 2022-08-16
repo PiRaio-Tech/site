256308e9e3d6fcf7f639ac1cb645044bc9d7c239
Marcus Paulo <marcuspsferreira@gmail.com>
2022-08-16 09:57:52 -0300

    header e alguns detalhes

---
+++ assets/scripts/main.js
@@ -0,0 +1,14 @@
+const btnRedes = document.querySelector(".btnRedes") 
+const listaRedes = document.querySelector(".listaRedes") 
+
+let redes = false
+
+btnRedes.addEventListener('click', () => {
+    if (redes){
+        listaRedes.style = 'display: none'
+        redes = false
+    }else{
+        listaRedes.style = 'display: inline-block'
+        redes = true
+    }
+})  
